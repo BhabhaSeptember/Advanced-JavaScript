d597ccd72a843b3b82ca5c3da7ca3c6f0f64c3a7
BhabhaSeptember <septemberbhabha@gmail.com>
2024-07-24 08:28:26 +0200

    commit

---
+++ Chapter10/Chapter_Projects/ch10.js
@@ -0,0 +1,140 @@
+"use strict";
+
+//**************************COLLAPSING ACCORDION**************/
+
+// const titles = document.querySelectorAll(".title");
+// const myTexts = document.querySelectorAll(".myText");
+
+// titles.forEach((title) => {
+//     title.addEventListener("click", (t) => {
+//         console.log(title.nextElementSibling);
+//         remover();
+//         title.nextElementSibling.classList.toggle("active");
+
+//     } );
+// });
+// function remover() {
+//     myTexts.forEach((text) => {
+//         text.classList.remove("active");
+//     })
+// }
+
+//**************************INTERACTIVE VOTING SYSTEM**************/
+
+// let myArray = ["Paul", "Steve", "Jabu", "Pravesh", "Niniola"];
+
+// let input = document.getElementById("addFriend");
+// let addButton = document.getElementById("addNew");
+// let output = document.getElementById("output");
+
+// addButton.addEventListener("click", () => {
+//     let newFriend = input.value;
+//     myArray.push(newFriend);
+//     adder(newFriend, myArray.length, 0);
+// });
+
+// function build() {
+//     myArray.forEach((friend, index) => {
+//         adder(friend, index, 0);
+//     });
+// };
+// window.onload = build();
+
+// function adder(friendName, index, counter) {
+//     const tr = document.createElement("tr");
+//     const td1 = document.createElement("td");
+//     const td2 = document.createElement("td");
+//     const td3 = document.createElement("td");
+
+//     td1.classList.add("box");
+//     td1.textContent = index + 1;
+//     td2.textContent = friendName;
+//     td3.textContent = counter;
+
+//     tr.append(td1);
+//     tr.append(td2);
+//     tr.append(td3);
+
+//     tr.addEventListener("click", () => {
+//         console.log(tr.lastChild);
+//         let value = Number(tr.lastChild.textContent);
+//         value++;
+//         tr.lastChild.textContent = value;
+//     })
+//     output.appendChild(tr);
+// };
+
+//**************************HANGMAN GAME*******************/
+
+
+const game = { cur: "", solution: "", puzz: [], total: 0 };
+const myWords = ["Butter", "Salt", "Chutney", "Barbecue"];
+const score = document.querySelector(".score");
+const puzzle = document.querySelector(".puzzle");
+const letters = document.querySelector(".letters");
+const btn = document.querySelector("button");
+btn.addEventListener("click", startGame);
+function startGame() {
+  if (myWords.length > 0) {
+    btn.style.display = "none";
+    game.puzz = [];
+    game.total = 0;
+    game.cur = myWords.shift();
+    game.solution = game.cur.split("");
+    builder();
+  } else {
+    score.textContent = "No More Words.";
+  }
+}
+function createElements(elType, parentEle, output, cla) {
+  const temp = document.createElement(elType);
+  temp.classList.add("boxE");
+  parentEle.append(temp);
+  temp.textContent = output;
+  return temp;
+}
+function updateScore() {
+  score.textContent = `Total Letters Left : ${game.total}`;
+  if (game.total <= 0) {
+    console.log("game over");
+    score.textContent = "Game Over";
+    btn.style.display = "block";
+  }
+}
+function builder() {
+  letters.innerHTML = "";
+  puzzle.innerHTML = "";
+  game.solution.forEach((lett) => {
+    let div = createElements("div", puzzle, "-", "boxE");
+    if (lett == " ") {
+      div.style.borderColor = "white";
+      div.textContent = " ";
+    } else {
+      game.total++;
+    }
+    game.puzz.push(div);
+    updateScore();
+  });
+  for (let i = 0; i < 26; i++) {
+    let temp = String.fromCharCode(65 + i);
+    let div = createElements("div", letters, temp, "box");
+    let checker = function (e) {
+      div.style.backgroundColor = "#ddd";
+      div.classList.remove("box");
+      div.classList.add("boxD");
+      div.removeEventListener("click", checker);
+      checkLetter(temp);
+    };
+    div.addEventListener("click", checker);
+  }
+}
+function checkLetter(letter) {
+  console.log(letter);
+  game.solution.forEach((ele, index) => {
+    if (ele.toUpperCase() == letter) {
+      game.puzz[index].textContent = letter;
+      game.total--;
+      updateScore();
+    }
+  });
+}
--- Chapter10/index.js
+++ Chapter10/index.js
@@ -15,22 +15,17 @@
 // console.dir(document.body.children.forest.children.tree2.previousElementSibling);
 // console.dir(document.body.children.forest.children.tree1.nextElementSibling);
 
-
 //**********************SELECTING ELEMENTS AS OBJECTS**********/
 
 // console.dir(document.body.children.greeting);
-
 
 //CHANGING INNER TEXT
 
 // document.body.children.greeting.innerText = "Bye!";
 
-
 //CHANGING INNER HTML
 
 // document.body.children.greeting.innerHTML = "<b>Bye!</b>";
-
-
 
 //**********************ACCESSING ELEMENTS IN DOM**********/
 
@@ -38,7 +33,6 @@
 // let div2 = document.getElementById("two");
 // div2.innerHTML = "<i><b>Hey!</b></i>";
 // console.log(div2);
-
 
 //ACCESSING ELEMENTS BY TAGNAME
 // let divs = document.getElementsByTagName("div");
@@ -50,11 +44,9 @@
 // let heading = document.getElementsByTagName("h1");
 // console.log(heading);
 
-
 //ACCESSING ELEMENTS BY CLASSNAME
 // let theClass = document.getElementsByClassName("example");
 // console.log(theClass);
-
 
 //ACCESSING ELEMENTS WITH CSS SELECTOR
 //QUERY SELECTOR()
@@ -71,7 +63,6 @@
 // let divs = document.querySelectorAll("div");
 // console.log(divs);
 
-
 //*****************************ELEMENT CLICK HANDLER***************/
 
 // function stop() {
@@ -82,14 +73,12 @@
 //     alert("Ouch! Please Stop!");
 // };
 
-
 //*******************************THIS AND THE DOM******************/
 
 // function reveal(e) {
 //     console.log(e);
 //     console.log(e.parentElement);
 // }
-
 
 //****************************MANIPULATING ELEMENT STYLE******************/
 
@@ -104,14 +93,12 @@
 //     };
 // };
 
-
 // function rainbowify() {
 //     let divs = document.getElementsByTagName("div");
 //     for(let i = 0; i < divs.length; i++) {
 //         divs[i].style.backgroundColor = divs[i].id;
 //     };
 // };
-
 
 //****************************CHANGING CLASSES OF AN ELEMENT******************/
 //ADDING CLASSES TO ELEMENTS
@@ -126,12 +113,10 @@
 //     document.getElementById("shape").classList.remove("blue");
 // };
 
-
 // //TOGGLING CLASSES
 // function changeVisibility() {
 //     document.getElementById("shape").classList.toggle("hide");
 // };
-
 
 //****************************MANIPULATING ATTRIBUTES**********************/
 
@@ -141,7 +126,6 @@
 //     e.setAttribute("id", "new");
 //     e.setAttribute("class", "circle");
 // };
-
 
 //***************************EVENT LISTENERS ON ELEMENTS**********************/
 
@@ -156,11 +140,135 @@
 //     this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 // };
 
-
 //***************************CREATING NEW ELEMENTS**********************/
 
-function addRandomNumber() {
-    let element = document.createElement("p");
-    element.innerText = Math.floor(Math.random() * 100);
-    document.body.appendChild(element); 
-};
+// function addRandomNumber() {
+//     let element = document.createElement("p");
+//     element.innerText = Math.floor(Math.random() * 100);
+//     document.body.appendChild(element);
+// };
+
+//----------------------------------------------------------------------------------
+
+//******************************PRACTICE EXERCISE 10.1************************/
+
+// console.dir(document);
+
+// //Output third list item
+// console.dir(document.body.children[0].children[0].children[0].children[2]);
+// console.log("Third List Item: ",document.body.children[0].children[0].children[0].children[2]);
+
+// //Output next node or element
+// console.dir(document.body.children[0].children[0].children[0].children[2].previousElementSibling);
+// console.log("Previous Element Sibling: ",document.body.children[0].children[0].children[0].children[2].previousElementSibling);
+
+//******************************PRACTICE EXERCISE 10.2************************/
+
+// let p = document.getElementById("paragraph");
+// console.log(p);
+
+//******************************PRACTICE EXERCISE 10.3************************/
+
+// //Empty array
+// let array = [];
+
+// //Selecting divs by tag name
+// let divs = document.getElementsByTagName("div");
+
+// //Push divs into empty array
+// array.push(divs);
+// console.log(array);
+
+// //Output Middle Element
+// let middle = array[0][1];
+// console.log(middle.innerHTML);
+
+//******************************PRACTICE EXERCISE 10.4************************/
+
+// //Selecting page elements by class
+// let elements = document.getElementsByClassName("same");
+// console.log(elements);
+
+// //Output elements
+// console.log(elements[0]);
+// console.log(elements[1]);
+// console.log(elements[2]);
+
+//******************************PRACTICE EXERCISE 10.5************************/
+
+// let firstOccurrence = document.querySelector(".same");
+// console.log(firstOccurrence);
+
+//******************************PRACTICE EXERCISE 10.6************************/
+
+// let all = document.querySelectorAll(".same");
+// console.log(all);
+
+// all.forEach( (e, i) => {
+//     console.log(i, e);
+// });
+
+//******************************PRACTICE EXERCISE 10.7************************/
+
+// function message(e) {
+//     console.dir(e.textContent);
+// };
+
+//******************************PRACTICE EXERCISE 10.8************************/
+
+// let names = ["Asanda", "Bhabha", "Boitumelo", "Happy", "Juan", "Lucky", "Michael", "Sharon", "Vhukhudo"];
+
+// let message = document.getElementById("message");
+// let output = document.getElementById("output");
+
+// function build() {
+
+//     let html = "<table> <h1> Students Table </h1>";
+//     for (let i = 0; i < names.length; i++) {
+//         html += `<tr class ="box" data-row="${i + 1}" data-name="${names[i]}" onclick="getData(this)">
+//         <td> ${names[i]} </td> </tr>`;
+//     }
+//     html += "</table>";
+
+//     document.getElementById("output").innerHTML = html;
+// };
+
+// window.onload = function () {
+//     build()
+// }
+
+// function getData(e) {
+//     let dataRow = e.getAttribute("data-row");
+//     let dataName = e.getAttribute("data-name");
+//     message.innerHTML = `${dataName} is in row number ${dataRow}`;
+// };
+
+//******************************PRACTICE EXERCISE 10.9************************/
+
+// let buttons = document.querySelectorAll("button");
+// console.log(buttons);
+
+// buttons.forEach((button) => {
+//   function output() {
+//     console.log(this.textContent);
+//   }
+//   button.addEventListener("click", output);
+// });
+
+
+//******************************PRACTICE EXERCISE 10.10************************/
+
+let message = document.getElementById("message");
+let addItem = document.getElementById("addItem");
+let addButton = document.getElementById("addNew");
+let output = document.getElementById("output");
+let sList = document.getElementById("sList");
+
+addButton.addEventListener("click", addOne);
+
+function addOne() {
+    let list = document.createElement("li");
+    let item = document.getElementById("addItem").value;
+    list.appendChild(document.createTextNode(item));
+    document.getElementById("sList").appendChild(list);
+}
