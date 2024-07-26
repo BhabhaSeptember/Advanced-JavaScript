8282227a441d206966d0694a60758d4fce42c9d2
BhabhaSeptember <septemberbhabha@gmail.com>
2024-07-26 08:30:01 +0200

    commit

--- Chapter12/index.js
+++ Chapter12/index.js
@@ -256,3 +256,7 @@
 // let strdog = JSON.stringify(dog);
 // console.log(typeof strdog);
 // console.log(strdog);
+
+
+//--------------------------------------------------------------------
+
--- Chapter9/Chapter_Projects/ch9.js
+++ Chapter9/Chapter_Projects/ch9.js
@@ -1,24 +1,25 @@
 "use strict";
 
-// let output = document.querySelector(".output");
-// let mainList = document.querySelector("ul");
-// mainList.id = "mainList";
+let output = document.querySelector(".output");
+let mainList = document.querySelector("ul");
+mainList.id = "mainList";
 
-// let divs = document.querySelectorAll("div");
+let divs = document.querySelectorAll("div");
 
-// let divsArray = [];
+let divsArray = [];
+console.log(divsArray);
+//
+for (let i = 0; i < divs.length; i++) {
+  divsArray.push(divs[i]);
 
-// //
-// for (let i = 0; i < divs.length; i++) {
-//     divsArray.push(divs[i]);
-//     console.log(divsArray);
-//     console.log(divs[i].tagName);
+  console.log(divs[i].tagName);
 
-//  divs[i].id = "id" + (i + 1);
-//  if (i % 2) { //(is index divisible by 2?)
-//  divs[i].style.color = "red"; //even numbers
-//  } else {
-//  divs[i].style.color = "blue"; //odd numbers
-//  }
-// }
+  divs[i].id = "id" + (i + 1);
 
+  if (i % 2) {
+    //(is index divisible by 2?)
+    divs[i].style.color = "red"; //even numbers
+  } else {
+    divs[i].style.color = "blue"; //odd numbers
+  }
+}
